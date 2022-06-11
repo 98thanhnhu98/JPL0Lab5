d8a900f1268d10c2df2bf3d07459671e9fb06467
98thanhnhu98 <97713475+98thanhnhu98@users.noreply.github.com>
2022-06-11 21:44:32 +0700

    first commit

---
+++ par3/bai1/Cylinder.java
@@ -0,0 +1,30 @@
+package JPL0Lab5.par3.bai1;
+
+public class Cylinder extends Circle {
+    private double height;
+    public  Cylinder(){
+        super();
+        height = 1.0;
+    }
+    public Cylinder(double height){
+        super();
+       this.height = height;
+    }
+    public Cylinder(double height, double radius){
+        super(radius);
+        this.height = height;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public double setHeight(double height){
+        this.height = height;
+        return this.height;
+    }
+    public double getVolume(){
+        return  super.getArea()*height;
+    }
+    public double getSTP(){
+        return (super.getArea() * 2) + getVolume();
+    }
+}
---
+++ par3/bai1/TestCylinder.java
@@ -0,0 +1,12 @@
+package JPL0Lab5.par3.bai1;
+
+public class TestCylinder {
+    public static void main(String[] args) {
+        Cylinder c = new Cylinder();
+        System.out.println("radius :" + c.getRadius() + "\n"
+                + "height :" + c.getHeight() + "\n"
+                + "Area :" + c.getArea() + "\n"
+                + "volume :" + c.getVolume() + "\n"
+                + "STP :"  + c.getSTP());
+    }
+}
